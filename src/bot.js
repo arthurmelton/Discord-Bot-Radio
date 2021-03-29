dfdc0c9b95a827741a690e3cab1f0322a05717d8
Arthur Melton <amtitan@icloud.com>
2021-03-29 17:08:04 -0400

    can play streams from url

---
+++ src/bot.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const { Client } = require('discord.js');
+const client = new Client();
+const PREFIX = "!R";
+
+client.on('ready', () => {
+console.log(`${client.user.tag} bot is on`);
+	client.user.setActivity(`${PREFIX}help`, {
+		type: 'WATCHING'
+	})
+		.then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+		.catch(console.error);
+})
+
+client.on("message", (message) => {
+	if (message.author.bot === true) return;
+	if (!message.content.startsWith(PREFIX)) return;
+	console.log(`[${message.author.tag}]: ${message.content}`);
+	const [commandName, ...args] = message.content
+		.trim()
+		.substring(PREFIX.length)
+		.split(/\s+/);
+	if (commandName == "play") {
+		const { voice } = message.member;
+		if (!voice.channel) {
+			message.reply("You have to be in a voice channel");
+			return;
+		}
+		const options = {seek: 0, volume: 1};
+		voice.channel.join().then(connection => {
+			const stream = args[0];
+			const dispatcher = connection.play(stream, options);
+			client.on('voiceStateUpdate', (oldMember, newMember) => {
+				if (oldMember.serverDeaf !== null) {
+					if (oldMember.serverDeaf === newMember.serverDeaf && oldMember.serverMute === newMember.serverMute && oldMember.selfDeaf == newMember.selfDeaf && oldMember.selfMute === newMember.selfMute && oldMember.selfVideo === newMember.selfVideo && oldMember.streaming === newMember.streaming) {} else {
+						return
+					}
+				}
+				if (oldMember.guild.channels.cache.get(oldMember.channelID)) {
+					if (oldMember.guild.channels.cache.get(oldMember.channelID).members.size === 1) {
+						oldMember.guild.channels.cache.get(oldMember.channelID).leave();
+						return;
+					}
+				}
+			})
+		})
+	}	
+})
+
+
+
+client.login(process.env.DISCORD_BOT_TOKEN);
